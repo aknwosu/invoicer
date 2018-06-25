04926889468cacad37f602c0a8c89c920c802ba5
AK Nwosu <akunna.nwosu@andela.com>
2018-06-25 09:05:07 +0100

    babel setup

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-var express = require('express')
-var app = express()
+import express from 'express';
+import logger from 'morgan';
 
+var app = express()
+app.use(express.urlencoded({ extended: true }))
+app.use(logger('dev'));
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello Invoicer')
 })
 
 app.listen(4000)
