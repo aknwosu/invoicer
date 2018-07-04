1a52fc0d41be09c9e5a9f6ce99ff68ead8f1d054
AK Nwosu <akunna.nwosu@andela.com>
2018-07-04 19:21:10 +0100

    add routes to root file controllers

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 import express from 'express';
 import logger from 'morgan';
+import user from './routes/user';
 
 const app = express();
+
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(logger('dev'));
+
+app.use('/api/v1/users', user);
+
+
 app.get('/', (req, res) => {
   res.send('Hello Invoicer');
 });
